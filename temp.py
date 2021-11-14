8c7c8e7f371454054bfc616b18486fb08dc6bbdd
Sudhanva Rajesh <sudhanva0001@gmail.com>
2021-11-15 00:14:42 +0530

    dsdsg

--- temp.py
+++ temp.py
@@ -5,13 +5,17 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 
 @app.route('/', methods = ['POST','GET'])
 def home():
     return render_template('temp.html',)
 
 @app.route('/login', methods = ['POST','GET'])
-def home():
+def login():
     return render_template('login.html',)
 
 if __name__ == '__main__':
