b9d9cb8d416e50abe31f39526eeacd6856121003
Sudhanva Rajesh <sudhanva0001@gmail.com>
2021-11-15 00:46:48 +0530

    login.html

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, request, render_template
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__)
+app = Flask(__name__,static_folder='templates')
 app.config['DEBUG'] = True
 
 
@@ -10,8 +10,8 @@ app.config['DEBUG'] = True
 def home():
     return render_template('temp.html',)
 
-@app.route('/Login/login.html', methods = ['POST','GET'])
-def home():
+@app.route('/login', methods = ['POST','GET'])
+def login():
     return render_template('login.html',)
 
 if __name__ == '__main__':
