350b759e111da019d748b7607079e5aad26c1cbe
Sudhanva Rajesh <sudhanva0001@gmail.com>
2021-11-14 23:49:53 +0530

    added temp.html

--- temp.py
+++ temp.py
@@ -3,12 +3,16 @@ import os
 from flask import Flask, request, render_template
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
-@app.route('/r', methods = ['POST','GET'])
+
+@app.route('/', methods = ['POST','GET'])
 def home():
-    
     return render_template('temp.html',)
 
+@app.route('/login', methods = ['POST','GET'])
+def home():
+    return render_template('login.html',)
 
 if __name__ == '__main__':
     app.run(debug=True)
