0df1144f59b4186d95af73187b51190b6ce61f72
Sudhanva Rajesh <sudhanva0001@gmail.com>
2021-11-17 00:03:17 +0530

    added query html

--- temp.py
+++ temp.py
@@ -6,6 +6,8 @@ app = Flask(__name__,static_folder='templates')
 app.config['DEBUG'] = True
 
 
+import psycopg2
+
 @app.route('/', methods = ['POST','GET'])
 def home():
     return render_template('temp.html',)
@@ -14,5 +16,51 @@ def home():
 def login():
     return render_template('login.html',)
 
+@app.route('/query', methods = ['POST','GET'])
+def query():
+    con=psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="2511"
+    )
+    cur=con.cursor()
+    cur.execute("""SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'""")
+    tables=cur.fetchall()
+    con.commit()
+    con.close()
+    return render_template('query.html',tables=tables)
+
+@app.route('/queryResult', methods = ['POST','GET'])
+def queryResult():
+    
+    command=request.form["command"]
+    table=request.form["table"]
+    if command=="select *":
+        query=command+" from "+table+';'
+    elif command=="insert into":
+        query=command+" from "+table+';'
+    
+    elif command=="alter table":
+        action=request.form["action"]
+        query=command+" "+table+" "+action+';'
+    
+    elif command=="update":
+        query=command+" from "+table+';'
+    
+    con=psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="2511"
+    )
+    cur=con.cursor()
+    cur.execute(query)
+    rows=cur.fetchall()
+    con.commit()
+    con.close()
+    return render_template('queryResult.html',rows=rows)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
