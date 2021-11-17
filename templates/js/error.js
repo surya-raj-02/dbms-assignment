c6c2736a141c95feb5b4dbf801c830572a59e65e
surya-raj-02 <suryasandeepraj2002@gmail.com>
2021-11-18 01:15:53 +0530

    error.js edited to remove weird words xD

--- templates/js/error.js
+++ templates/js/error.js
@@ -1,9 +1,9 @@
     // Here are the different messages we'll use for creating the 500 displayable message
 const messages = [
-  ['Whoops.', 'Oops.', 'Excuse me.', 'Oh Dear.', 'Well poo.', 'Hm...', 'This is awkward.', 'Well gosh!'],
+  ['Whoops.', 'Oops.', 'Excuse me.', 'Oh Dear.', 'Well...', 'Hm...', 'This is awkward.', 'Well gosh!'],
   ['It appears', 'Looks like', 'Unfortunately,', 'It just so happens', 'Sadly,', 'Seemingly from nowhere'],
-  ['there was an error.', 'we goofed up.', 'a bad thing happend.', 'the server crashed.', 'a bug appeared.', 'someone did a naughty.', 'pixies got into the server!', 'the server threw a tantrum.', 'the website had a bad day.', 'our code pooped out.'],
-  ['Sorry.', 'Apologies.', 'Our bad.', 'Sad day.', 'We are quite contrite.', 'Beg pardon.']
+  ['there was an error.', 'we goofed up.', 'a bad thing happend.', 'the server crashed.', 'a bug appeared.', 'someone did a mistake.', 'pixies got into the server!', 'the server threw a tantrum.', 'the website had a bad day.', 'our code broke.'],
+  ['Sorry.', 'Apologies.', 'Our bad.', 'Sad day.', 'We are quite contrite.', 'Beg pardon.','This was fun.']
 ];
 
 // These are the different elements we'll be populating. They are in the same order as the messages array
